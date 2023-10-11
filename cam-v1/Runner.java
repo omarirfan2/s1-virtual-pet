f3ed071ed0a1daba9ce70b5509677ca2bf1f0ea5
Omar Gochyyev <omarirfang24@ausdk12.org>
2023-10-11 11:31:12 -0700

    w

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,7 +1,27 @@
 public class Runner {
-    public static void main(String[] args) {
-      VirtualPet v = new VirtualPet();
-      v.feed();
-      v.sleep();
+ 
+public Runner(){
+      VirtualPet p = new VirtualPet();
+      p.exercise();
+      takeABeat(5000);
+      p.feed();
+      String ans = getAnswer("How are you?");
+      System.out.println(ans);
+      if (ans.equals("Good")){
+        
+      }
 }
+
+public void takeABeat(int millieseconds) {
+try {
+        Thread.sleep(1000); //milliseconds
+    } catch(Exception e){
+    
+    }
+}
+
+    public static void main(String[] args) {
+      new Runner();
+}
+
 }
