ee93472a2494492309c6df600486c793db562058
Omar Gochyyev <omarirfang24@ausdk12.org>
2023-10-13 11:30:37 -0700

    week 10

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,55 +1,16 @@
 
 import javax.swing.*;
 
-public class Runner {
 
-public Runner(){
+public class Runner(){
+      public static void main(String[] args) {
       VirtualPet p = new VirtualPet();
-      takeABeat(1000);
-      p.exercise();
       p.feed();
       p.cry();
       p.play();
 
-      }
 
-public int getCategoryResponse(String q) {
-    Object[] options = {"Yes, please",
-                "No way!"};
-    int n = JOptionPane.showOptionDialog(
-        new JFrame(),
-        "test"
-        "test2"
-    )
-}
-
-
-
-
-    public void takeABeat(int milliseconds) {
-        try{
-            Thread.sleep(milliseconds);
-
-        } catch (Exception e) {}
-    }
-
-public String getResponse(String s) {
-    String a = (String)JOptionPane.showInputDialog(
-                    new JFrame(),
-                    "How was your day?",
-                    "test",
-                    JOptionPane.PLAIN_MESSAGE
-
-        );
-        if (s.equals("good")) {
-            return "thats good";
-        }
-}
-    
-
-    public static void main(String[] args) {
-        
-      
-}
 
 }
+}
+
--- cam-v1/VirtualPet.java
+++ cam-v1/VirtualPet.java
@@ -3,18 +3,56 @@
  * @author Cam
  * @author ?
  */
+
+ 
+
 public class VirtualPet {
     
     VirtualPetFace face;
-    int hunger = 0;   // how hungry the pet is.
+    int hunger = 0;
+    int happiness = 1;   // how hungry the pet is.
     
     // constructor
     public VirtualPet() {
         face = new VirtualPetFace();
-        face.setImage("normal");
-        face.setMessage("Hello.");
+        face.setImage("tired");
+        face.setMessage("The baby is starting to get hungry...");
+    }
+
+
+
+
+
+    public boolean fed(String f){
+        Integer i = Integer.parseInt(f);
+        if (i <= 3) {
+            return true;
+        }
+        return false;
     }
     
+public void defender(String a) {
+        if (a.equals("Upset")) {
+            face.setMessage("Oh no! You're baby is upset!");
+            face.setImage("cry");
+        }
+
+        if (a.equals("")) {
+            face.setMessage("");
+            face.setImage("");
+        }
+
+    }
+
+
+public void takeABeat(int milliseconds) {
+    try {
+        Thread.sleep(milliseconds); // milliseconds
+    } catch (Exception e) {
+    }
+}
+
+
     public void feed() {
         if (hunger > 10) {
             hunger = hunger - 10;
